b54c0241f3e8ce4ff542af8e10f2e4d12010e645
Paul Campbell <pcampbell@kemitix.net>
2022-01-31 16:58:44 +0000

    react-scrimba-dark-mode-toggle 2022-01-31 16:58:44 track-work snapshot

--- react/scrimba-learnreact/04-info-site-dark-mode-toggle/src/components/Navbar.jsx
+++ react/scrimba-learnreact/04-info-site-dark-mode-toggle/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar(props) {
         >
             <img 
                 className="nav--logo_icon"
-                src="./images/react-icon-small.png"
+                src="/images/react-icon-small.png"
             />
             <h3 className="nav--logo_text">ReactFacts</h3>
             
