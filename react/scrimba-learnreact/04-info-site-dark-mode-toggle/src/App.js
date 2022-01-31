0d3e040eea1b35e3f599a23ac9e9a5330efc06d2
Paul Campbell <pcampbell@kemitix.net>
2022-01-31 16:48:45 +0000

    react-scrimba-dark-mode-toggle 2022-01-31 16:48:45 track-work snapshot

--- react/scrimba-learnreact/04-info-site-dark-mode-toggle/src/App.js
+++ react/scrimba-learnreact/04-info-site-dark-mode-toggle/src/App.js
@@ -1,17 +1,3 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Main from './components/Main';
-
-function App() {
-  return (
-    <div className="container">
-      <Navbar />
-      <Main />
-    </div>
-  );
-}
-
-export default App;
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 
