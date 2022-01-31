4a14363073b2b2aab75819991286511e4d638bb2
Paul Campbell <pcampbell@kemitix.net>
2022-01-31 16:51:06 +0000

    react-scrimba-dark-mode-toggle 2022-01-31 16:51:06 track-work snapshot

--- react/scrimba-learnreact/04-info-site-dark-mode-toggle/src/App.js
+++ react/scrimba-learnreact/04-info-site-dark-mode-toggle/src/App.js
@@ -5,7 +5,7 @@ export default function App() {
     return (
         <div className="container">
             <Navbar />
-            <Main darkMode="true" />
+            <Main darkMode="dark" />
         </div>
     )
 }
