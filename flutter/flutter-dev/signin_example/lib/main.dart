561720997a69438f7a30d3ff2e59a2eea01915da
Paul Campbell <pcampbell@kemitix.net>
2022-04-05 20:34:58 +0100

    Enable sign in progress tracking

--- flutter/flutter-dev/signin_example/lib/main.dart
+++ flutter/flutter-dev/signin_example/lib/main.dart
@@ -66,9 +66,27 @@ class _SignUpFormState extends State<SignUpForm> {
     Navigator.of(context).pushNamed('/welcome');
   }
 
+  void _updateFormProgress() {
+    var progress = 0.0;
+    final controllers = [
+      _firstNameTextController,
+      _lastNameTextController,
+      _usernameTextController
+    ];
+    for (final controller in controllers) {
+      if (controller.value.text.isNotEmpty) {
+        progress += 1 / controllers.length;
+      }
+    }
+    setState(() {
+      _formProgress = progress;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
+      onChanged: _updateFormProgress,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -110,7 +128,7 @@ class _SignUpFormState extends State<SignUpForm> {
                     : Colors.blue;
               }),
             ),
-            onPressed: _showWelcomeScreen,
+            onPressed: _formProgress == 1 ?  _showWelcomeScreen : null,
             child: const Text('Sign up'),
           ),
         ],
