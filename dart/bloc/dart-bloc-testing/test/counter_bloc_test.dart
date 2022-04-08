67b487a3fe3d549dd25906b23307c8c69e0ae5fd
Paul Campbell <pcampbell@kemitix.net>
2022-04-08 07:12:02 +0100

    Add first blocTest s

--- dart/bloc/dart-bloc-testing/test/counter_bloc_test.dart
+++ dart/bloc/dart-bloc-testing/test/counter_bloc_test.dart
@@ -1,18 +1,28 @@
-//import 'package:bloc_test/bloc_test.dart';
-
-import 'package:test/scaffolding.dart';
+import 'package:bloc_test/bloc_test.dart';
 import 'package:test/test.dart';
 
 import '../bin/counter_bloc.dart';
 
 void main() {
-  group('CounterBlock', () {
-    late CounterBloc counterBlock;
+  group('CounterBloc', () {
+    late CounterBloc counterBloc;
     setUp(() {
-      counterBlock = CounterBloc();
+      counterBloc = CounterBloc();
     });
     test('initial state is 0', () {
-      expect(counterBlock.state, 0);
+      expect(counterBloc.state, 0);
     });
+    blocTest<CounterBloc, int>(
+      'emits [1] when CounterIncrementPressed is added',
+      build: () => counterBloc,
+      act: (bloc) => bloc.add(CounterIncrementPressed()),
+      expect: () => [1],
+    );
+    blocTest<CounterBloc, int>(
+      'emits [-1] when CounterDecrementPressed is added',
+      build: () => counterBloc,
+      act: (bloc) => bloc.add(CounterDecrementPressed()),
+      expect: () => [-1],
+    );
   });
 }
